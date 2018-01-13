7fa7d91256d2f7ef7cda3e446927b07bfb0a623b
Rafael <rafaelgoncalvesbarreira@gmail.com>
2018-01-12 23:45:46 -0300

    creating mock api

---
+++ TransportPlusLtda/Controllers/Api/GpsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TransportPlusLtda.Models.Api;
+
+namespace TransportPlusLtda.Controllers.Api
+{
+    /// <summary>
+    /// This is a Mock API. All values will be random
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/Gps")]
+    public class GpsController : Controller
+    {
+        [HttpGet("{id}")]
+        public PositionViewModel Get(Guid id)
+        {
+            Random random = new Random();
+
+            return new PositionViewModel
+            {
+                Id = Guid.NewGuid(),
+                Latitude = (random.NextDouble() * 2.0 - 1.0) * 90,
+                Longitude = (random.NextDouble() * 2.0 - 1.0) * 180
+            };
+        }
+    }
+}
